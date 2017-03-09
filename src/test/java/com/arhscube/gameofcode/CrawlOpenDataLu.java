5f82ebf3ccc5d39cfc2bf6c97eb44b7352b5147d
Gerald Even <gerald.even@arhs-cube.com>
2017-03-09 17:18:17 +0100

    Improved the crawler to compute next URL to crawl from here.

--- src/test/java/com/arhscube/gameofcode/CrawlOpenDataLu.java
+++ src/test/java/com/arhscube/gameofcode/CrawlOpenDataLu.java
@@ -1,6 +1,7 @@
 package com.arhscube.gameofcode;
 
 import java.net.URL;
+import java.util.Random;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,17 +15,20 @@ public class CrawlOpenDataLu {
 	static Logger log = LoggerFactory.getLogger(CrawlOpenDataLu.class);
 	private static final int TIMEOUT = 5 * 1000;
 	@Test
-	public void crawlPageOne()throws Exception{
-		URL url = new URL("https://data.public.lu/en/datasets/?page=1");
+	public void crawlARandomPage()throws Exception{
+		int page=new Random(System.currentTimeMillis()).nextInt(20)+1;
+		URL url = new URL("https://data.public.lu/en/datasets/?page="+page);
 		Document doc = Jsoup.parse(url, TIMEOUT);
-		log.info("Doc '{}' parsed ",doc.title());
+		log.info("Doc '{}' parsed from '{}'",doc.title(),url);
 		Elements datasets = doc.select("li[class~=dataset-result]");
 		for(Element dataset : datasets){
 			Element a = dataset.getElementsByTag("a").first();
 			String link = a.attr("href");
+			URL datasetUrl = new URL(url,link);
 			String title = a.attr("title");
 			String description = dataset.getElementsByClass("result-description").first().text();
 			log.info("crawled {}\n {}\n href='{}'",title, description,link);
+			log.info("Could get more from '{}'",datasetUrl);
 		}
 	}
 }
