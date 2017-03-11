daa60ffe00061977a309f3d8e17e5c7039ba9b23
borellda <daniel.borella@arhs-cube.com>
2017-03-11 15:24:06 +0100

    init tbd

--- src/main/java/tripelstore/TripleStoreService.java
+++ src/main/java/tripelstore/TripleStoreService.java
@@ -5,19 +5,24 @@ import org.apache.jena.tdb.TDBFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by borellda on 3/11/2017.
  */
-@Component
+@Service
 public class TripleStoreService {
     /* The Logger */
     private static final Logger log = LoggerFactory.getLogger(TripleStoreService.class);
     private static final String TROPLESTORE = new String("./tbd");
 
-    private Dataset dataset;
+    private Dataset store;
+
+    public TripleStoreService(){
+        this.init();
+    }
 
     public void init(){
-         this.dataset = TDBFactory.createDataset(TROPLESTORE) ;
+         this.store = TDBFactory.createDataset(TROPLESTORE) ;
     }
 }
