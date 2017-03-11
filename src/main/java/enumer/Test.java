1bbead44815201dee9d82245f8c5a6f72ca59d66
falia <fatonalia@gmail.com>
2017-03-11 14:27:27 +0100

    modified structure + map

---
+++ src/main/java/enumer/Test.java
@@ -0,0 +1,7 @@
+package enumer;
+
+/**
+ * Created by aliafa on 3/11/2017.
+ */
+public enum Test {
+}
---
+++ src/main/java/model/Resource.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by aliafa on 3/11/2017.
+ */
+public class Resource {
+}
