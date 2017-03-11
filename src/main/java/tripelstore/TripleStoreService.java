d5eedb356e3a3cbb56ec8ebf42f6a29d9b06a8e6
borellda <daniel.borella@arhs-cube.com>
2017-03-11 16:37:13 +0100

    init tbd

--- src/main/java/tripelstore/TripleStoreService.java
+++ src/main/java/tripelstore/TripleStoreService.java
@@ -1,6 +1,8 @@
 package tripelstore;
 
 import org.apache.jena.query.Dataset;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.tdb.TDBFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,19 +12,17 @@ import org.springframework.stereotype.Service;
 /**
  * Created by borellda on 3/11/2017.
  */
-@Service
 public class TripleStoreService {
     /* The Logger */
     private static final Logger log = LoggerFactory.getLogger(TripleStoreService.class);
     private static final String TROPLESTORE = new String("./tbd");
+    private Model dcatap;
 
-    private Dataset store;
-
-    public TripleStoreService(){
-        this.init();
+    public TripleStoreService() {
+        this.dcatap = RDFDataMgr.loadModel("./GoC_example.owl") ;
     }
 
-    public void init(){
-         this.store = TDBFactory.createDataset(TROPLESTORE) ;
-    }
+
+
+
 }
---
+++ src/test/java/com/arhscube/gameofcode/CreateLocalripleStoreTestT.java
@@ -0,0 +1,22 @@
+package com.arhscube.gameofcode;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tripelstore.TripleStoreService;
+
+/**
+ * Created by borellda on 3/11/2017.
+ */
+public class CreateLocalripleStoreTestT {
+    /* The Logger */
+    private static final Logger log = LoggerFactory.getLogger(CreateLocalripleStoreTestT.class);
+
+
+    @Test
+    public void createModeltest(){
+        TripleStoreService store = new TripleStoreService();
+    }
+}
+
+
